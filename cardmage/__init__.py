3c2970fb6951fdc34374ef4372ff3f8bddd2e491
xenomorphis <wachter.ma@web.de>
2022-08-26 21:53:04 +0200

    Feature: Wraps all input operations in a try-except-block & adds status messages

--- cardmage/__init__.py
+++ cardmage/__init__.py
@@ -42,42 +42,52 @@ def cl_main() -> None:
     # working_dir = os.getcwd()
 
     for card in args.path:
-        blueprint = toml.load(dir_path(base_dir + settings['paths']['cards'] + card))
-        # 2. Load the necessary preset .toml files based on blueprint data (fonts, layouts)
-        font_preset = toml.load(dir_path(base_dir + settings['paths']['fonts'] + blueprint['text']['preset'] + ".toml"))
-        layout = toml.load(dir_path(base_dir + settings['paths']['layouts'] + blueprint['layout']['type'] + ".toml"))
+        try:
+            blueprint = toml.load(dir_path(base_dir + settings['paths']['cards'] + card))
+            print("Build '" + blueprint['meta']['edition'] + "-" + blueprint['meta']['id'] + ".png' started.")
 
-        template = Image(filename=dir_path(base_dir + settings['paths']['layouts'] + layout['template']['file']))
+            # 2. Load the necessary preset .toml files based on blueprint data (fonts, layouts)
+            font = toml.load(dir_path(base_dir + settings['paths']['fonts'] + blueprint['text']['preset'] + ".toml"))
+            layout = toml.load(dir_path(base_dir + settings['paths']['layouts'] + blueprint['layout']['type'] + ".toml"))
 
-        if layout['image']['use_vertical']:
-            if args.print:
-                card_image = Image(
-                    filename=dir_path(base_dir + settings['paths']['images'] + blueprint['image']['source_vertical_hd']))
+            template = Image(filename=dir_path(base_dir + settings['paths']['layouts'] + layout['template']['file']))
+
+            if layout['image']['use_vertical']:
+                if args.print:
+                    card_image = Image(
+                        filename=dir_path(base_dir + settings['paths']['images'] + blueprint['image']['source_vertical_hd']))
+                else:
+                    card_image = Image(
+                        filename=dir_path(base_dir + settings['paths']['images'] + blueprint['image']['source_vertical']))
             else:
-                card_image = Image(
-                    filename=dir_path(base_dir + settings['paths']['images'] + blueprint['image']['source_vertical']))
+                if args.print:
+                    card_image = Image(
+                        filename=dir_path(base_dir + settings['paths']['images'] + blueprint['image']['source_hd']))
+                else:
+                    card_image = Image(
+                        filename=dir_path(base_dir + settings['paths']['images'] + blueprint['image']['source']))
+
+        except FileNotFoundError as error:
+            print(error)
+            print("- Build '" + blueprint['meta']['edition'] + "-" + blueprint['meta']['id'] + ".png' failed.")
+            continue
+
         else:
-            if args.print:
-                card_image = Image(
-                    filename=dir_path(base_dir + settings['paths']['images'] + blueprint['image']['source_hd']))
-            else:
-                card_image = Image(
-                    filename=dir_path(base_dir + settings['paths']['images'] + blueprint['image']['source']))
+            # 3. Use wand to construct the final card (save intermediate files in _build)
+            with Color(layout['template']['background']) as bg:
+                current = Image(width=layout['template']['size'][0], height=layout['template']['size'][1], background=bg)
 
-        # 3. Use wand to construct the final card (save intermediate files in _build)
-        with Color(layout['template']['background']) as bg:
-            current = Image(width=layout['template']['size'][0], height=layout['template']['size'][1], background=bg)
+            with Drawing() as draw:
+                layer = template.clone()
+                draw.composite(operator='atop', left=0, top=0, width=layer.width, height=layer.height, image=layer)
+                draw(current)
+                uts = str(int(time.time()))
+                current.save(filename=buildpath + uts + "-template.png")
 
-        with Drawing() as draw:
-            layer = template.clone()
-            draw.composite(operator='atop', left=0, top=0, width=layer.width, height=layer.height, image=layer)
-            draw(current)
-            uts = str(int(time.time()))
-            current.save(filename=buildpath + uts + "-template.png")
-
-        # 4. Use wand to place text onto card (save intermediate files in _build)
-        # 5. Save image in dist
-        current.save(filename=str(distpath + blueprint['meta']['edition'] + "-" + blueprint['meta']['id'] + ".png"))
+            # 4. Use wand to place text onto card (save intermediate files in _build)
+            # 5. Save image in dist
+            current.save(filename=str(distpath + blueprint['meta']['edition'] + "-" + blueprint['meta']['id'] + ".png"))
+            print("- Build '" + blueprint['meta']['edition'] + "-" + blueprint['meta']['id'] + ".png' completed.")
 
     # 6. Remove _build and it's contents
     shutil.rmtree(buildpath)
@@ -87,7 +97,7 @@ def dir_path(string):
     if os.path.exists(string):
         return string
     else:
-        raise NotADirectoryError(string)
+        raise FileNotFoundError(string)
 
 
 def vartest(content, filename) -> None:
