128ccdc07fa97a74f615f556a14d9eafb19901f8
xenomorphis <wachter.ma@web.de>
2022-08-26 15:20:20 +0200

    Feature: Adds creation of a new empty card, adding the template layer and saving it to disk

--- cardmage/__init__.py
+++ cardmage/__init__.py
@@ -4,8 +4,11 @@ import argparse
 import json
 import os
 import shutil
+import time
 import toml
-import wand
+from wand.color import Color
+from wand.drawing import Drawing
+from wand.image import Image, COMPOSITE_OPERATORS
 
 
 def cl_main() -> None:
@@ -14,19 +17,33 @@ def cl_main() -> None:
     """
 
     arg_parser = argparse.ArgumentParser(description='Cardmage open-source card builder')
-    arg_parser.add_argument("path", nargs="+", help="Path to the card's root TOML file", type=dir_path)
+    arg_parser.add_argument("path", nargs="+", help="Path to the card's root TOML file")
     arg_parser.add_argument("-p", "--print", help="Render card in print quality", default=False, action="store_true")
+    arg_parser.add_argument("-t", "--test", help="Use test settings", default=False, action="store_true")
 
     args = arg_parser.parse_args()
     # args.print enthält die Info, ob der print-Switch gesetzt ist oder nicht
 
     blueprint = dict()
-    working_dir = os.getcwd()
+
+    if args.test:
+        settings = toml.load(dir_path("../testdata/settings.toml"))
+    else:
+        settings = toml.load(dir_path("./settings.toml"))
+
+    base_dir = settings['paths']['base']
+    # working_dir = os.getcwd()
 
     for card in args.path:
-        blueprint = toml.load(card)
-        buildpath = os.path.join(working_dir, '_build')
-        distpath = os.path.join(working_dir, 'dist')
+        blueprint = toml.load(dir_path(base_dir + settings['paths']['cards'] + card))
+        # 2. Load the necessary preset .toml files based on blueprint data (fonts, layouts)
+        font_preset = toml.load(dir_path(base_dir + settings['paths']['fonts'] + blueprint['text']['preset'] + ".toml"))
+        layout = toml.load(dir_path(base_dir + settings['paths']['layouts'] + blueprint['layout']['type'] + ".toml"))
+
+        template = Image(filename=dir_path(base_dir + settings['paths']['layouts'] + layout['template']['file']))
+
+        buildpath = os.path.join(base_dir, '_build/')
+        distpath = os.path.join(base_dir, 'dist/')
 
         if not os.path.exists(buildpath):
             os.mkdir(buildpath)
@@ -34,13 +51,23 @@ def cl_main() -> None:
         if not os.path.exists(distpath):
             os.mkdir(distpath)
 
-        print(blueprint)
-        vartest(blueprint, card)
+        # vartest(blueprint, card)
 
-        # 2. Load the necessary preset .toml files based on blueprint data (fonts, layouts)
         # 3. Use wand to construct the final card (save intermediate files in _build)
+        with Color(layout['template']['background']) as bg:
+            current = Image(width=layout['template']['size'][0], height=layout['template']['size'][1], background=bg)
+
+        with Drawing() as draw:
+            layer = template.clone()
+            draw.composite(operator='atop', left=0, top=0, width=layer.width, height=layer.height, image=layer)
+            draw(current)
+            uts = str(int(time.time()))
+            current.save(filename=buildpath + uts + "-template.png")
+
         # 4. Use wand to place text onto card (save intermediate files in _build)
         # 5. Save image in dist
+        current.save(filename=str(distpath + blueprint['meta']['edition'] + "-" + blueprint['meta']['id'] + ".png"))
+
         # 6. Remove _build and it's contents
         shutil.rmtree(buildpath)
 
@@ -52,7 +79,7 @@ def dir_path(string):
         raise NotADirectoryError(string)
 
 
-def vartest(content, filename):
+def vartest(content, filename) -> None:
     filename_raw = filename.partition('.toml')
 
     with open(filename_raw[0] + ".output.json", "w") as outfile:
